64e39619ddca10f7cd2b59ced7dc1594be8f0eb1
fau <4474909+aufau@users.noreply.github.com>
2025-03-24 23:57:10 +0100

    Fix gamepad scrolling or moving camera when console is open

--- src/sdl/sdl_input.cpp
+++ src/sdl/sdl_input.cpp
@@ -1792,7 +1792,7 @@ static void IN_PadMove(int eventTime)
 
 	SDL_GameControllerUpdate();
 
-	if (Key_GetCatcher() & (KEYCATCH_UI | KEYCATCH_CGAME)) {
+	if (Key_GetCatcher() & (KEYCATCH_UI | KEYCATCH_CGAME | KEYCATCH_CONSOLE)) {
 		IN_PadMoveUI(eventTime);
 		if (in_gamepadTriggersAxis->integer == 0) {
 			// send SE_KEY events for triggers so that binding works
